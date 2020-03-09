8a3d9f4b046846ba557e761db3b102cb875ad44c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-09 11:03:07 +0000

    Add copyright logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import copyrightLogo from '../../images/copyright-logo.png';
 
 class Footer extends Component {
   render() {
     return (
       <div className='footer-component'>
         <div className='footer-left'>
+          <img id='copyright-logo' src={copyrightLogo}></img>
           <p data-test='footer-licence'>Andrea Diotallevi 2020</p>
         </div>
         <div className='footer-right'>
