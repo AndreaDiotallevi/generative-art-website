ecb197d7c07b37c2fc3a89ff092cd56a77cbd4a6
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-09 23:27:41 +0000

    Refactor tests

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -3,25 +3,27 @@ import { shallow } from 'enzyme';
 import Footer from './Footer';
 
 describe('Footer', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Footer />);
+  })
+
   it('should render without errors', () => {
-    const wrapper = shallow(<Footer />);
     expect(wrapper.exists()) === true
   })
 
   it('should render the copyright logo', () => {
-    const wrapper = shallow(<Footer />);
     const img = wrapper.find({ id: 'copyright-logo'});
     expect(img.props().src).toEqual('copyright-logo.png');
   })
 
   it('should render Andrea Diotallevi 2020', () => {
-    const wrapper = shallow(<Footer />);
     const p = wrapper.find({ id: 'footer-copyright'});
     expect(p.text()).toEqual('Andrea Diotallevi 2020');
   })
 
   it('should render the instagram logo', () => {
-    const wrapper = shallow(<Footer />);
     const img = wrapper.find({ id: 'instagram-logo'});
     expect(img.props().src).toEqual('instagram-logo.png');
   })
--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -3,19 +3,22 @@ import { shallow } from 'enzyme';
 import Navbar from './Navbar';
 
 describe('Navbar', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Navbar />);
+  })
+
   it('should render without errors', () => {
-    const wrapper = shallow(<Navbar />);
     expect(wrapper.exists()) === true
   })
 
   it('should render ANDREA DIOTALLEVI', () => {
-    const wrapper = shallow(<Navbar />);
     const h1 = wrapper.find({ id: 'navbar-title' });
     expect(h1.text()).toEqual('ANDREA DIOTALLEVI');
   })
 
   it('should render home and about navbar links', () => {
-    const wrapper = shallow(<Navbar />);
     const homeLink = wrapper.find({ id: 'navbar-home' });
     const aboutLink = wrapper.find({ id: 'navbar-about' });
     expect(homeLink.text()).toEqual('home');
