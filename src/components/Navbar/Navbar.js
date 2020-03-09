e74726e318e78c49b2709deb2eadbea7cdccecab
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-09 11:25:27 +0000

    Update css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,11 +9,11 @@ class Navbar extends Component {
           <h1 data-test='navbar-title'>ANDREA DIOTALLEVI</h1>
         </div>
         <div className='navbar-right'>
-          <Link to='/'>
-            <p className='navbar-link' data-test='navbar-home'>home</p>
+          <Link to='/' className='navbar-link'>
+            <p data-test='navbar-home'>home</p>
           </Link>
-          <Link to='/about'>
-            <p className='navbar-link' data-test='navbar-about'>about</p>
+          <Link to='/about' className='navbar-link'>
+            <p data-test='navbar-about'>about</p>
           </Link>
         </div>
       </div>
