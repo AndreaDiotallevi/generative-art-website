3d93e3538a666641abfdfadf4eb743399cc5e509
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-10 21:19:45 +0000

    Update css to fix footer position

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
+    <div id="app">
       <BrowserRouter>
         <Navbar />
         <Route path='/' exact component={Artworks}></Route>
