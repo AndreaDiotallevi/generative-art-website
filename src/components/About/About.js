eb928788ec016cae761a998d3793211a88f32875
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-10 20:41:45 +0000

    Add about title, photo and description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import staggeredPortrait from '../../images/artworks/staggered-portrait.png';
 
 class About extends Component {
   render() {
     return (
       <div id='about-component'>
-        About
+        <div id='about-container'>
+          <div>
+            <img id='profile-image' src={staggeredPortrait}></img>
+          </div>
+          <div>
+            <h3 id='about-title'>about</h3>
+            <p id='about-description'>Software developer with a background in music and architecture, fascinated by the intersection between art and technology. For each new work, I design a custom algorithm capable of generating a sequence of unique, but aesthetically related images.</p>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -9,7 +9,13 @@ describe('About', () => {
     wrapper = shallow(<About />);
   })
 
-  it('should render without errors', () => {
-    expect(wrapper.length).toEqual(1);
+  it('should render the text about', () => {
+    const p = wrapper.find({ id: 'about-title'});
+    expect(p.text()).toEqual("about");
+  })
+
+  it('should render the description', () => {
+    const p = wrapper.find({ id: 'about-description'});
+    expect(p.text()).toEqual("Software developer with a background in music and architecture, fascinated by the intersection between art and technology. For each new work, I design a custom algorithm capable of generating a sequence of unique, but aesthetically related images.");
   })
 })
