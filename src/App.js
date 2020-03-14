ae5683e097af55762cf0f4e099c5dc3be86c948b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-14 11:36:21 +0000

    Create artowkr route in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar'
-import Artworks from './components/Artworks/Artworks'
-import About from './components/About/About'
-import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar';
+import Artworks from './components/Artworks/Artworks';
+import Artwork from './components/Artwork/Artwork';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Route path='/' exact component={Artworks}></Route>
         <Route path='/about' component={About}></Route>
+        <Route path='/artworks/:title' component={Artwork}></Route>
         <Footer />
       </BrowserRouter>
     </div>
