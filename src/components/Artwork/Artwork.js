b7264844e9aa8b9d704935bff2d36578a583dd8b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-14 11:30:42 +0000

    Add alt attribute to image

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -6,7 +6,7 @@ function Artwork() {
     return (
       <div id='artwork-component'>
         <div id='artwork-container'>
-          <img className='artwork-page-image' src={require(`../../assets/artworks/${title}-750x750.png`)}></img>
+          <img className='artwork-page-image' src={require(`../../assets/artworks/${title}-750x750.png`)} alt={title}></img>
           <h3 id='artwork-page-title'>{title.replace('-', ' ')}</h3>
         </div>
       </div>
