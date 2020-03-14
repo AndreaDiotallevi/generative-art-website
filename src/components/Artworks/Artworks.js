43bcda8d2661f2661c239c4c3e8a16242a4be3a5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-14 20:19:03 +0000

    Refactor images import

--- src/components/Artworks/Artworks.js
+++ src/components/Artworks/Artworks.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import agateStoneImage from '../../assets/agate-stone-750x750.png';
-import marbleLakeImage from '../../assets/marble-lake-750x750.png';
-import fluidImage from '../../assets/fluid-750x750.png';
-import daisyImage from '../../assets/daisy-750x750.png';
-import whiteCherryImage from '../../assets/white-cherry-750x750.png';
-import blackHoleImage from '../../assets/black-hole-750x750.png';
+import imageLoader from '../../images';
 
 class Artworks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      artworks: [
-        { title: 'agate stone', imageUrl: agateStoneImage },
-        { title: 'marble lake', imageUrl: marbleLakeImage },
-        { title: 'fluid', imageUrl: fluidImage },
-        { title: 'daisy', imageUrl: daisyImage },
-        { title: 'white cherry', imageUrl: whiteCherryImage },
-        { title: 'black hole', imageUrl: blackHoleImage }
-      ]
+      artworks: imageLoader()
     };
   }
 
   render() {
+    console.log(this.state.artworks)
     return (
       <div id='artworks-component'>
         <ul id='artworks-container'>
-          {this.state.artworks.map((artwork, index) => (
-          <li className='artwork' key={index}>
+          {this.state.artworks.map((artwork) => (
+          <li className='artwork' key={artwork.id}>
             <Link to={`/artworks/${artwork.title.replace(' ', '-')}`} className='artwork-link'>
-            <img id={`artwork-image-${index}`} className='artwork-image' src={artwork.imageUrl} alt={artwork.title}></img>
-              <p id={`artwork-title-${index}`} className='artwork-title'>{artwork.title}</p>
+              <img id={`artwork-image-${artwork.id}`} className='artwork-image' src={`${artwork.src}`} alt={artwork.title}></img>
+              <p id={`artwork-title-${artwork.id}`} className='artwork-title'>{artwork.title}</p>
             </Link>
           </li>
           ))}
