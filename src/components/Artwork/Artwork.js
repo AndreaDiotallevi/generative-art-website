1f7c386a0ad8b0ad3ec858ff3ba03148602903a8
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-14 12:52:13 +0000

    Add tests for the Artwork component

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
-function Artwork() {
-  const { title } = useParams();
-    return (
-      <div id='artwork-component'>
-        <div id='artwork-container'>
-          <img className='artwork-page-image' src={require(`../../assets/artworks/${title}-750x750.png`)} alt={title}></img>
-          <h3 id='artwork-page-title'>{title.replace('-', ' ')}</h3>
-        </div>
+function Artwork(props) {
+  const title = props.match.params.title;
+
+  return (
+    <div id='artwork-component'>
+      <div id='artwork-container'>
+        <img id='artwork-page-image' className='artwork-page-image' src={require(`../../assets/artworks/${title}-750x750.png`)} alt={title}></img>
+        <h3 id='artwork-page-title'>{title.replace('-', ' ')}</h3>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Artwork;
