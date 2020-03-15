b06343a0ec1616b5bbcada02f84f39a57f34c5b5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-15 09:45:32 +0000

    Fix tests for artworks component

--- src/components/Artworks/Artworks.js
+++ src/components/Artworks/Artworks.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import imageLoader from '../../images';
+import artworks from '../../utils/artworks';
 
-class Artworks extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      artworks: imageLoader()
-    };
-  }
-
-  render() {
-    console.log(this.state.artworks)
-    return (
-      <div id='artworks-component'>
-        <ul id='artworks-container'>
-          {this.state.artworks.map((artwork) => (
-          <li className='artwork' key={artwork.id}>
-            <Link to={`/artworks/${artwork.title.replace(' ', '-')}`} className='artwork-link'>
-              <img id={`artwork-image-${artwork.id}`} className='artwork-image' src={`${artwork.src}`} alt={artwork.title}></img>
-              <p id={`artwork-title-${artwork.id}`} className='artwork-title'>{artwork.title}</p>
-            </Link>
-          </li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+function Artworks() {
+  return (
+    <div id='artworks-component'>
+      <ul id='artworks-container'>
+        {artworks.map((artwork) => (
+        <li className='artwork' key={artwork.id}>
+          <Link to={`/artworks/${artwork.title.replace(' ', '-')}`} className='artwork-link'>
+            <img id={`artwork-image-${artwork.id}`} className='artwork-image' src={require(`../../assets/${artwork.title.replace(' ', '-')}-750x750.png`)} alt={artwork.title}></img>
+            <p id={`artwork-title-${artwork.id}`} className='artwork-title'>{artwork.title}</p>
+          </Link>
+        </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Artworks;
---
+++ src/utils/artworks.js
@@ -0,0 +1,10 @@
+const artworks = [
+  { id: 1, title: 'agate stone' },
+  { id: 2, title: 'marble lake' },
+  { id: 3, title: 'fluid' },
+  { id: 4, title: 'daisy' },
+  { id: 5, title: 'white cherry' },
+  { id: 6, title: 'black hole' }
+]
+
+export default artworks;
