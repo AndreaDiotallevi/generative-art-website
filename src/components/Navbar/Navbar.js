6d186b91566942d414485d886a6d374ccd9861f5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-16 19:26:33 +0000

    Add medium blog hyperlink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,15 +14,16 @@ function Navbar(props) {
           <h1 id='navbar-title' className={isHome() && 'hidden'}>ANDREA DIOTALLEVI</h1>
         </div>
         <div className='navbar-right'>
-          <Link to='/' className='navbar-link' onClick={props.onClick}>
+          <Link to='/' className={`navbar-link ${isHome() && 'text-when-home'}`} onClick={props.onClick}>
             <p id='navbar-home' className={`navbar-text ${isHome() && 'text-when-home'}`}>Home</p>
           </Link>
-          <Link to='/artworks' className='navbar-link' onClick={props.onClick}>
+          <a href='https://medium.com/@andreadiotallevi' id='medium-hyperlink' target="_blank" rel="noopener noreferrer" className={`navbar-link navbar-text ${isHome() && 'text-when-home'}`}>Blog</a>
+          <Link to='/artworks' className={`navbar-link ${isHome() && 'text-when-home'}`} onClick={props.onClick}>
             <p id='navbar-artworks' className={`navbar-text ${isHome() && 'text-when-home'}`}>Artworks</p>
           </Link>
-          <Link to='/about' className='navbar-link' onClick={props.onClick}>
+          {/* <Link to='/about' className='navbar-link' onClick={props.onClick}>
             <p id='navbar-about' className={`navbar-text ${isHome() && 'text-when-home'}`}>About</p>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
