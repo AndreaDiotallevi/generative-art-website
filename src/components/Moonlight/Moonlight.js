8cb32977228815b5c047b7011dc1ec8ccbb04548
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-17 15:23:20 +0000

    Amend p5 sketch to re-run when user clicks

--- src/components/Moonlight/Moonlight.js
+++ src/components/Moonlight/Moonlight.js
@@ -27,10 +27,10 @@ class Moonlight extends React.Component {
   };
 
   mouseClicked = p5 => {
-    // p5.noiseSeed(p5.random());
-    // p5.background(56, 68, 76);
-    // p5.redraw();
-    p5.noLoop();
+    p5.noiseSeed(p5.random());
+    p5.background(56, 68, 76);
+    p5.redraw();
+    // p5.noLoop();
   }
 
   windowResized = p5 => {
