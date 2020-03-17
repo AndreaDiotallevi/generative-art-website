eb5dc787407b7edef9c4d796e697d0f71a042c12
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-17 13:49:47 +0000

    Refactor Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,11 +14,11 @@ const Navbar = props => {
           <h1 id='navbar-title' className={isHome() ? 'hidden' : undefined}>ANDREA DIOTALLEVI</h1>
         </div>
         <div className='navbar-right'>
-          <Link to='/' className={`navbar-link ${isHome() ? 'text-when-home' : undefined}`} onClick={props.onClick}>
+          <Link to='/' className={`navbar-link ${isHome() ? 'text-when-home' : undefined}`}>
             <p id='navbar-home' className={`navbar-text ${isHome() ? 'text-when-home' : undefined}`}>home</p>
           </Link>
           <a href='https://medium.com/@andreadiotallevi' id='medium-hyperlink' target="_blank" rel="noopener noreferrer" className={`navbar-link navbar-text ${isHome() ? 'text-when-home' : undefined}`}>blog</a>
-          <Link to='/artworks' className={`navbar-link ${isHome() ? 'text-when-home' : undefined}`} onClick={props.onClick}>
+          <Link to='/artworks' className={`navbar-link ${isHome() ? 'text-when-home' : undefined}`}>
             <p id='navbar-artworks' className={`navbar-text ${isHome() ? 'text-when-home' : undefined}`}>art</p>
           </Link>
         </div>
