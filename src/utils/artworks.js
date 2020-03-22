177787c4d306a9c0e03234c1175cce5693a01120
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-22 13:26:12 +0000

    Add artworks

--- src/utils/artworks.js
+++ src/utils/artworks.js
@@ -4,7 +4,13 @@ const artworks = [
   { id: 3, variations: 4, title: 'fluid', description: 'A series of fluid forms made with circles and gradients.' },
   { id: 4, variations: 1, title: 'daisy', description: 'A mathematical approach for drawing a daisy.' },
   { id: 5, variations: 1, title: 'white cherry', description: 'A fractal experimentation with processing.' },
-  { id: 6, variations: 1, title: 'black hole', description: 'A radial combination of Bezier curves.' }
+  { id: 6, variations: 1, title: 'black hole', description: 'A radial combination of Bezier curves.' },
+  { id: 7, variations: 1, title: 'divergent flow', description: '' },
+  { id: 8, variations: 2, title: 'imaginary topography', description: '' },
+  { id: 9, variations: 4, title: 'textile craters', description: '' },
+  { id: 11, variations: 1, title: 'lava sea', description: '' },
+  { id: 12, variations: 1, title: 'waving fabric', description: '' },
+  { id: 12, variations: 2, title: 'water currents', description: '' }
 ]
 
 export default artworks;
