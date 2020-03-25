fbbe0100af426dc70bd0cf782593281cd385e483
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-25 14:03:59 +0000

    Amend css of homepage

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -14,8 +14,13 @@ describe('Home', () => {
     expect(h3.text()).toEqual('Hello! My name is Andrea.');
   })
 
-  it('should render the home page paragraph', () => {
-    const p = wrapper.find({ id: 'home-page-paragraph' })
-    expect(p.text()).toEqual('I am a Software Engineer, with a background in music and architecture, fascinated by the intersection between art and technology.')
+  it('should render the home page paragraph 1', () => {
+    const p = wrapper.find({ id: 'home-page-paragraph-1' })
+    expect(p.text()).toEqual('I am a software engineer, architect and pianist, fascinated by the intersection between art and technology.')
+  })
+
+  it('should render the home page paragraph 2', () => {
+    const p = wrapper.find({ id: 'home-page-paragraph-2' })
+    expect(p.text()).toEqual('For each new work, I design a custom algorithm capable of generating a sequence of unique, but aesthetically related images.')
   })
 })
