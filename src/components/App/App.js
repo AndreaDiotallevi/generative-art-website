8257fd8f74c12516134caf3d08fd7586c2df4559
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-21 13:31:41 +0100

    Re-organise files

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Artwork from "../Artwork/Artwork";
+import Artworks from "../Artworks/Artworks";
+import Footer from "../Footer/Footer";
+import Home from "../Home/Home";
+import Navbar from "../Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Route component={Navbar}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/art" exact component={Artworks}></Route>
+        <Route path="/art/:title" component={Artwork}></Route>
+        <Route component={Footer}></Route>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
