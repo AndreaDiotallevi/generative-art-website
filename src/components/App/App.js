633463253c18f06000f6e166e29db66d2dd5370f
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-21 13:37:35 +0100

    Add ProjectList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import ProjectList from "../ProjectList/ProjectList";
 import Artwork from "../Artwork/Artwork";
 import Artworks from "../Artworks/Artworks";
 import Footer from "../Footer/Footer";
@@ -13,6 +14,7 @@ const App = () => {
       <BrowserRouter>
         <Route component={Navbar}></Route>
         <Route path="/" exact component={Home}></Route>
+        <Route path="/dev" exact component={ProjectList}></Route>
         <Route path="/art" exact component={Artworks}></Route>
         <Route path="/art/:title" component={Artwork}></Route>
         <Route component={Footer}></Route>
---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ProjectGif from "./ProjectGif";
+import ProjectOverview from "./ProjectOverview";
+import projects from "../utils/projects";
+
+const ProjectList = () => {
+  const renderList = () => {
+    return projects.map((project) => (
+      <div className="project-list-item" key={project.name}>
+        <ProjectGif project={project} />
+        <ProjectOverview project={project} />
+      </div>
+    ));
+  };
+
+  return <div className="project-list-component">{renderList()}</div>;
+};
+
+export default ProjectList;
