1d0a6fe8f1c2b7a07740a3a3a5f2ada7be3b9dae
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-21 20:54:57 +0100

    Add blog anchor tag

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,10 +9,6 @@ const Navbar = (props) => {
     }
   };
 
-  const isActive = (route) => {
-    return props.location.pathname.split("/").includes(route);
-  };
-
   return (
     <div id="navbar-component" className={`navbar-component ${getClassName()}`}>
       <div id="navbar-container">
@@ -27,24 +23,26 @@ const Navbar = (props) => {
           <Link to="/portfolio">
             <p
               id="navbar-portfolio"
-              className={`navbar-text ${getClassName("portfolio")}`}
+              className={`navbar-text ${getClassName()}`}
             >
               Dev Portfolio
             </p>
           </Link>
+          <a
+            href="https://medium.com/@andreadiotallevi"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`blog-link ${getClassName()}`}
+          >
+            Blog
+          </a>
           <Link to="/art">
-            <p
-              id="navbar-artworks"
-              className={`navbar-text ${getClassName("art")}`}
-            >
+            <p id="navbar-artworks" className={`navbar-text ${getClassName()}`}>
               Generative Art
             </p>
           </Link>
           <Link to="/contact">
-            <p
-              id="navbar-contact"
-              className={`navbar-text ${getClassName("contact")}`}
-            >
+            <p id="navbar-contact" className={`navbar-text ${getClassName()}`}>
               Contact
             </p>
           </Link>
