c0768bdba020f4ba0da834ffc22997d698e70827
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-23 09:20:37 +0100

    Amend artwork component and tests

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import artworks from '../../utils/artworks';
+import React from "react";
+import artworks from "../../utils/artworks";
 
 class Artwork extends React.Component {
   title = this.props.match.params.title;
-  artwork = artworks.find(artwork => artwork.title === this.title.replace('-', ' '));
+  artwork = artworks.find(
+    (artwork) => artwork.title === this.title.replace("-", " ")
+  );
 
   componentDidMount() {
     window.scrollTo(0, 0);
@@ -12,25 +14,33 @@ class Artwork extends React.Component {
   renderImages() {
     let images = [];
     for (let i = 0; i < this.artwork.variations; i++) {
-      images.push(<img key={`${this.title}-${i}`} id={`artwork-page-image-${i}`} className='artwork-page-image' src={require(`../../assets/${this.title}-${i}.png`)} alt={this.title}></img>)
+      images.push(
+        <img
+          key={`${this.title}-${i}`}
+          id={`artwork-page-image-${i}`}
+          className="artwork-page-image"
+          src={require(`../../assets/${this.title}-${i}.png`)}
+          alt={this.title}
+        ></img>
+      );
     }
     return images;
   }
 
   render() {
     return (
-      <div id='artwork-component'>
-        <div id='artwork-container'>
-          <div id='artwork-page-images'>
-            {this.renderImages()}
-          </div>
-          <div id='artwork-page-info'>
-            <h3 id='artwork-page-title'>{this.title.replace('-', ' ').toUpperCase()}</h3>
-            <p id='artwork-page-description'>{this.artwork.description}</p>
+      <div id="artwork-component">
+        <div id="artwork-container">
+          <div id="artwork-page-images">{this.renderImages()}</div>
+          <div id="artwork-page-info">
+            <h3 id="artwork-page-title">
+              {this.title.replace("-", " ").toUpperCase()}
+            </h3>
+            <p id="artwork-page-description">{this.artwork.description}</p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/Artwork/Artwork.test.js
+++ src/components/Artwork/Artwork.test.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Artwork from './Artwork';
+import React from "react";
+import { shallow } from "enzyme";
+import Artwork from "./Artwork";
 
-jest.mock('../../utils/artworks');
+jest.mock("../../utils/artworks");
 
-describe('Artwork', () => {
+describe("Artwork", () => {
   let wrapper;
-  let props = { match: { params: { title: 'test-artwork' } } };
+  let props = { match: { params: { title: "test-artwork" } } };
 
   beforeEach(() => {
-    wrapper = shallow(<Artwork {...props}/>);
-  })
+    wrapper = shallow(<Artwork {...props} />);
+  });
 
-  it('should render the artwork page image', () => {
-    const img = wrapper.find({ id: 'artwork-page-image-0'});
-    expect(img.props().src).toEqual('test-artwork-0.png');
-  })
+  it("should render the artwork page image", () => {
+    const img = wrapper.find({ id: "artwork-page-image-0" });
+    expect(img.props().src).toEqual("test-artwork-0.png");
+  });
 
-  it('should render the artwork page title', () => {
-    const h3 = wrapper.find({ id: 'artwork-page-title' } );
-    expect(h3.text()).toEqual('TEST ARTWORK');
-  })
+  it("should render the artwork page title", () => {
+    const h3 = wrapper.find({ id: "artwork-page-title" });
+    expect(h3.text()).toEqual("TEST ARTWORK");
+  });
 
-  it('should render the artwork description', () => {
-    const p = wrapper.find({ id: 'artwork-page-description' } );
-    expect(p.text()).toEqual('description');
-  })
-})
+  it("should render the artwork description", () => {
+    const p = wrapper.find({ id: "artwork-page-description" });
+    expect(p.text()).toEqual("description");
+  });
+});
