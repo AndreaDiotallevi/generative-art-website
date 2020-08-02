577b76005c1ae995d973d2e23c6728778ee79c68
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-08-02 18:00:32 +0100

    Delete test for App

--- src/components/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from './App';
-import { shallow } from 'enzyme';
-
-test('App renders without errors', () => {
-  let wrapper = shallow(<App />);
-  expect(wrapper.length).toEqual(1);
-});
